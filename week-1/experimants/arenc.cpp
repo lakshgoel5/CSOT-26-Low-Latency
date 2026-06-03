6a97cde4c52f72143124c472e683eaf9fd173342
Laksh Goel <lakshgoel541@gmail.com>
2026-06-03 16:48:14 +0900

    experiment

--- week-1/experimants/arenc.cpp
+++ week-1/experimants/arenc.cpp
@@ -1,11 +1,19 @@
+#include <cstddef>
+#include <new>
+#include <utility>
+#include <vector>
+
 class Arena {
-    std::vector<std::byte> buf;
+    std::vector<unsigned char> buf;
     size_t offset = 0;
     
 public:
+    // Explicit: Prevents implicit conversions from size_t to Arena
     explicit Arena(size_t cap) : buf(cap) {}
 
     template <typename T, typename... Args>
+    // typename... Args: Variadic templates - allows the function to accept any number of arguments
+    // Args&&... args: Forwarding references - allows the function to accept arguments by reference or rvalue reference
     T* alloc(Args&&... args) {
         // round up to alignof(T)
         size_t aligned = (offset + alignof(T) - 1) & ~(alignof(T) - 1);
